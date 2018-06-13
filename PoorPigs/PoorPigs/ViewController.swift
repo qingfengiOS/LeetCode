6a5cbe765f71ce678daa9058d07081c26000abae
qingfeng <qingfengiosdev@gmail.com>
2018-06-13 17:40:26 +0800

    add

---
+++ PoorPigs/PoorPigs/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  PoorPigs
+//
+//  Created by 李一平 on 2018/6/13.
+//  Copyright © 2018年 slwy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let resOfPig = poorPigs(100, 15, 60)
+        print(resOfPig)
+    }
+
+    func poorPigs(_ buckets: Int, _ minutesToDie: Int, _ minutesToTest: Int) -> Int {
+        if buckets == 1 {
+            return 0;
+        }
+        
+        let base = minutesToTest / minutesToDie + 1 //每头猪最多可测试的水桶数
+        var numberofPig = 1
+        var total = base
+        while total < buckets {
+            total = total * base
+            numberofPig += 1;
+        }
+        return numberofPig
+    }
+    
+}
+
