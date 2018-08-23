8b2cfacd48a97828a38d76c790ca9cc804bc743e
情风 <291753906@qq.com>
2018-08-23 14:37:48 +0800

    reverse words and sentense

---
+++ ReverseWords/ReverseWords/main.swift
@@ -0,0 +1,48 @@
+//
+//  main.swift
+//  ReverseWords
+//
+//  Created by qingfeng on 2018/8/23.
+//  Copyright © 2018年 qingfengiOS. All rights reserved.
+//
+
+import Foundation
+
+func swapElement<T>(_ chars: inout [T], _ p: Int, _ q: Int) {
+    (chars[p], chars[q]) = (chars[q], chars[p])
+}
+
+func reverseString<T>(_ chars: inout [T], start: Int, end: Int) {
+    var start = start
+    var end = end
+    while start < end {
+        swapElement(&chars, start, end)
+        start += 1
+        end -= 1
+    }
+}
+
+var chs = ["1","2","3"]
+reverseString(&chs, start: 0, end: chs.count - 1)
+print(chs)
+
+
+func reverseWords(_ strng: String?) -> String? {
+    guard strng != nil else {
+        return nil
+    }
+    var chars = Array(strng!.characters)
+    var start = 0
+    reverseString(&chars, start: 0, end: chars.count - 1)
+    
+    for i in 0 ..< chars.count {
+        if i == chars.count - 1 || chars[i + 1] == " " {
+            reverseString(&chars, start: start, end: i)
+            start = i + 2
+        }
+    }
+    return String(chars)
+}
+
+print(reverseWords("Hello, World!") as Any)
+
